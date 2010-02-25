7aea5784d737354b185cf108bf55d98a2f520ebc
David G. Holm <dgh3+github@netfang.net>
2010-02-24 20:43:38 -0500

    Added an Options button
    Added an Options button and connected it to the Options dialog,
    but it doesn't appear to detect the default user values that are
    handed to it, because all of the check boxes indicate undefined.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 import tkinter
 from tkinter import ttk
+from options import Options
+from user_options import UserOptions
 
 class Main:
     def __init__(self, parent):
@@ -11,29 +13,35 @@ class Main:
         self.init_widgets()
 
     def init_widgets(self):
+        self.user = UserOptions()
+        self.options = ttk.Button(self.root, command=self.options, text="Options", width=12)
+        self.options.grid(column=0, row=0, sticky='w')
         self.connect = ttk.Button(self.root, command=self.connect, text='Connect', width=12)
         self.disconnect = ttk.Button(self.root, command=self.disconnect, text='Disconnect', width=12)
-        self.connect.grid(column=0, row=0, sticky='w')
+        self.connect.grid(column=1, row=0, sticky='w')
         self.entry = ttk.Entry(self.root, width=90)
-        self.entry.grid(column=0, row=0, sticky='e')
+        self.entry.grid(column=2, row=0, sticky='e')
         self.txt = tkinter.Text(self.root, width=80, height=50)
-        self.txt.grid(column=0, row=1, sticky='nwes')
+        self.txt.grid(column=0, row=1, columnspan=3, sticky='nwes')
         sb = ttk.Scrollbar(command=self.txt.yview, orient='vertical')
-        sb.grid(column=1, row=1, sticky='ns')
+        sb.grid(column=3, row=1, sticky='ns')
         self.txt['yscrollcommand'] = sb.set
 
     def connect(self):
         self.insert('Connecting...')
-        self.disconnect.grid(column=0, row=0, sticky='w')
+        self.disconnect.grid(column=1, row=0, sticky='w')
         self.connect.grid_forget()
 
     def disconnect(self):
         self.insert('Disconnecting...')
-        self.connect.grid(column=0, row=0, sticky='w')
+        self.connect.grid(column=1, row=0, sticky='w')
         self.disconnect.grid_forget()
 
     def insert(self, text):
         self.txt.insert(tkinter.INSERT, text)
+
+    def options(self):
+        opt = Options(self, self.user)
 
 if __name__ == '__main__':
     main = Main(None)
